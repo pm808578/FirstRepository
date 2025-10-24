faba4b7985b34d657ae97fe3aeebf82097481b71
pm808578 <pm808578@gmail.com>
2025-10-24 08:39:55 +0530

    adding new file in new branch VikasBranch

---
+++ src/test/java/StringHardPrograms/AddNumbersInAStringCopy.java
@@ -0,0 +1,23 @@
+package StringHardPrograms;
+
+public class AddNumbersInAStringCopy {
+
+    static void main(String[] args) {
+        String str="Mghwuenng7fgg4hhff3gf9gbb8";
+        //op  --- 7+4+3+9+8==
+        System.out.println(7+4+3+9+8);
+        int sum=0;
+        for (char c:str.toCharArray())
+        {
+            if (Character.isDigit(c))
+            {
+                sum=sum+Character.getNumericValue(c);
+
+                //sum=sum+Integer.parseInt(String.valueOf(c));
+
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
